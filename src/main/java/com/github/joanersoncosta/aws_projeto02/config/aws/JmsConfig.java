7b0347eee3cafd56f68fcd95564239c9b4d65056
Joanerson Costa <joanersonc31@gmail.com>
2024-10-29 15:51:46 -0300

    feat: add connection factory config

---
+++ src/main/java/com/github/joanersoncosta/aws_projeto02/config/aws/JmsConfig.java
@@ -0,0 +1,41 @@
+package com.github.joanersoncosta.aws_projeto02.config.aws;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.metadata.DataSourcePoolMetadataProvidersConfiguration;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Session;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+    @Value("${aws.region}")
+    private String awsRegion;
+    private SQSConnectionFactory sqsConnectionFactory;
+
+    public DefaultJmsListenerContainerFactory defaultJmsListenerContainerFactory(){
+
+        sqsConnectionFactory = new SQSConnectionFactory(
+                new ProviderConfiguration(),
+                AmazonSQSClientBuilder.standard()
+                        .withRegion(awsRegion)
+                        .withCredentials(new DefaultAWSCredentialsProviderChain())
+                        .build());
+
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory((ConnectionFactory) sqsConnectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setConcurrency("2");
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+        return factory;
+    }
+}
